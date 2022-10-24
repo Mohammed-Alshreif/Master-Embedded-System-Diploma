031ebee789f4a4ebe47b9d360c87a406f7827722
momo <momo@hmail,com>
2022-10-24 12:17:56 +0200

    add_LCD_and_CONTROLES Signed-off-by: momo <momo@hmail,com>

---
+++ Unit8_TIMERS_ADC/STM32f103c6_SPWM3PHASE_DRIVER(with_LCD)/Debug/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/stm32_speed_DRIVER.d
@@ -0,0 +1,11 @@
+STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/stm32_speed_DRIVER.o: \
+ ../STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/stm32_speed_DRIVER.c \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/stm32_speed_DRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/GPIO_DRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/STM32f103c6.h
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/stm32_speed_DRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/GPIO_DRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/STM32f103c6.h:
---
+++ Unit8_TIMERS_ADC/STM32f103c6_SPWM3PHASE_DRIVER(with_LCD)/Debug/Src/main.d
@@ -0,0 +1,22 @@
+Src/main.o: ../Src/main.c \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/STM32f103c6.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/GPIO_DRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/STM32f103c6.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/stm32_speed_DRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/GPIO_DRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/stm32f103c6_ISR_DRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/LCD.h
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/STM32f103c6.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/GPIO_DRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/STM32f103c6.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/stm32_speed_DRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/GPIO_DRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/stm32f103c6_ISR_DRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/STM32f103c6_SPWM3PHASE_DRIVER/STM32f103c6_DRIVERS/STM32f103c6_DRIVERS/inc/LCD.h:
