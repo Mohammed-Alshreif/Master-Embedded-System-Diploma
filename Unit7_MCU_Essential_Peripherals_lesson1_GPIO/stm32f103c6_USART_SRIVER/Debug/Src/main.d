318ae83261772cf39f49180f27d5c75da4305652
momo <momo@hmail,com>
2022-10-29 20:18:59 +0200

    USART Signed-off-by: momo <momo@hmail,com>

---
+++ Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/Debug/Src/main.d
@@ -0,0 +1,16 @@
+Src/main.o: ../Src/main.c \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/stm32f103c6_USART_SRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/STM32f103c6.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/GPIO_DRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/stm32_speed_DRIVER.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/GPIO_DRIVER.h
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/stm32f103c6_USART_SRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/STM32f103c6.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/GPIO_DRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/stm32_speed_DRIVER.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/GPIO_DRIVER.h:
---
+++ Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/Debug/stm32f103c6_SRIVERS/keypad.d
@@ -0,0 +1,10 @@
+stm32f103c6_SRIVERS/keypad.o: ../stm32f103c6_SRIVERS/keypad.c \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/keypad.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/STM32f103c6.h \
+ C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/GPIO_DRIVER.h
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/keypad.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/STM32f103c6.h:
+
+C:/E/embedded/test/final\ simulation\ code\ for\ my\ assignments/Unit7_MCU_Essential_Peripherals_lesson1_GPIO/stm32f103c6_USART_SRIVER/stm32f103c6_SRIVERS/inc/GPIO_DRIVER.h:
