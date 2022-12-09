039b48596b88c3a99f5fe5970ac9c4a19f3308d3
momo <momo@hmail,com>
2022-12-09 11:27:53 +0200

    I2C_LCD

--- Unit7_MCU_Essential_Peripherals_lesson1_GPIO/I2C_LCD_DRIVER/STM32_DRIVERS/STM32f103c6_DRIVERS/STM32_I2C_LCD.c
+++ Unit7_MCU_Essential_Peripherals_lesson1_GPIO/I2C_LCD_DRIVER/STM32_DRIVERS/STM32f103c6_DRIVERS/STM32_I2C_LCD.c
@@ -14,16 +14,16 @@ void _12cdelay_ms(long x){
 void I2C_LCD_write_CMD(I2C_TypeDef* I2Cx,char CMD,uint16_t address){
 
 	unsigned char temp = 0x00;
-	uint8_t lcd = 0x0C;
+	uint8_t lcd = 0x0C;//1100  RS=0 RW=0 E=1 backlight =1
 	lcd &= 0x0F;
-	temp = (CMD & 0xF0);
-	lcd |= (temp| 0x04);
+	temp = (CMD & 0xF0);//4 MSB first
+	lcd |= (temp| 0x04);// 4 MSB of CMD + 1100  RS=0 RW=0 E=1 backlight =1
 
-	I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start)	;
+	I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);//send to i2C
 
 	_12cdelay_ms(10);
-	lcd &= ~(0x04);
-	I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);
+	lcd &= ~(0x04);//E=0
+	I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);// 4 MSB of CMD + 1100  RS=0 RW=0 E=0 backlight =1
 	_12cdelay_ms(10);
 
 
@@ -32,14 +32,14 @@ void I2C_LCD_write_CMD(I2C_TypeDef* I2Cx,char CMD,uint16_t address){
 	if(flag_init)
 	{
 
-		temp = ((CMD & 0x0F)<<4);
-		lcd &= 0x0F;
-		lcd |= temp;
-		lcd |= (0x04);
-		I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);
-		_12cdelay_ms(1);
-		lcd &= ~(0x04);
-		I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);
+		temp = ((CMD & 0x0F)<<4);//4 LSB first
+		lcd = 0x0C;//1100  RS=0 RW=0 E=1 backlight =1
+		lcd |= (temp| 0x04);// 4 LSB of CMD + 1100  RS=0 RW=0 E=1 backlight =1
+
+		I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);//send to i2C
+		_12cdelay_ms(10);
+		lcd &= ~(0x04);//E=0
+		I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);// 4 LSB of CMD + 1100  RS=0 RW=0 E=0 backlight =1
 		_12cdelay_ms(5);
 	}
 	_12cdelay_ms(2);
@@ -48,24 +48,25 @@ void I2C_LCD_Init(I2C_TypeDef* I2Cx,uint16_t address){
 	I2C_INIT(I2C1,8000000);
 	flag_init=0;
 	_12cdelay_ms(90);
-	I2C_LCD_write_CMD(I2Cx,0x20, address);
-	I2C_LCD_write_CMD(I2Cx,0x20, address);
+	I2C_LCD_write_CMD(I2Cx,0x03, address);//--- Initialize Sequence
+	I2C_LCD_write_CMD(I2Cx,0x03, address);
+	I2C_LCD_write_CMD(I2Cx,0x03, address);
+	I2C_LCD_write_CMD(I2Cx,0x20, address);//--- Return to Home
+	I2C_LCD_write_CMD(I2Cx,0x20, address);//--- Return to Home
 	I2C_LCD_write_CMD(I2Cx,0x80, address);// 2 line and 5x7 pixel N=1 f=0
-	_12cdelay_ms(1);
+	_12cdelay_ms(2);
 	I2C_LCD_write_CMD(I2Cx,0x00, address);
 	I2C_LCD_write_CMD(I2Cx,0xC0, address);//cerser
-	_12cdelay_ms(1);
-	I2C_LCD_write_CMD(I2Cx,0x00, address);
-	I2C_LCD_write_CMD(I2Cx,0x10, address);
 	_12cdelay_ms(2);
-
-	flag_init=1;
+	I2C_LCD_write_CMD(I2Cx,0x00, address);
+	I2C_LCD_write_CMD(I2Cx,0x10, address);//--- Clear LCD
+	_12cdelay_ms(30);
+flag_init=1;
 }
 void I2C_LCD_write_data(I2C_TypeDef* I2Cx ,char data,uint16_t address){
 
 	unsigned char temp = 0x00;
-	uint8_t lcd = 0x0D;
-	lcd &= 0x0F;
+	uint8_t lcd = 0x0D;//1101  RS=1 RW=0 E=1 backlight =1
 	temp = (data & 0xF0);
 	lcd |= (temp| 0x04);
 
@@ -78,12 +79,11 @@ void I2C_LCD_write_data(I2C_TypeDef* I2Cx ,char data,uint16_t address){
 
 
 	temp = ((data & 0x0F)<<4);
-	lcd &= 0x0F;
-	lcd |= temp;
-	lcd |= (0x04);
+	lcd = 0x0D;//1101  RS=1 RW=0 E=1 backlight =1
+	lcd |= (temp|0x04);
 	I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);
 	_12cdelay_ms(1);
-	lcd &= ~(0x04);
+	lcd &= ~(0x04);//E=0
 	I2C_Master_Send(I2Cx, &lcd, 1, address, WithStop, Start);
 	_12cdelay_ms(5);
 
