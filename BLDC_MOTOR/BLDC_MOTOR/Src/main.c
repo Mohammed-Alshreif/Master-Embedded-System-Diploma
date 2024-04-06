9346171f8df83fd1caff69ea4299dcb8e334062a
momo <momo@hmail,com>
2024-04-06 18:14:19 +0200

    Add encoder motor1 and motor2 and make the code more genaric

--- BLDC_MOTOR/BLDC_MOTOR/Src/main.c
+++ BLDC_MOTOR/BLDC_MOTOR/Src/main.c
@@ -9,12 +9,17 @@
 #include "stm32f103c6_ADC.h"
 #include "stm32f103c6_USART_SRIVER.h"
 #include "math.h"
+#include <string.h>
+//*******************************************************
+#define MOTOR_MASTER //select driver that works on the correct motor
+//#define Encoder_16_P //if it comment the number of pulses in one rotation is 31 pulses
 //*******************************************************
 #define clk 36000000                 //system clock
+#define SYSTEM_DELAY 90              //main delay
 #define NUM_OF_HALLSENSOR_PULS 30    //pulses per rotation
 #define MAX_PWM 2000                 //max motor PWM high
 #define MOTOR_PWM_LOW 800            //motor PWM LOW
-#define MAX_SPEED 2300              //MAX_SPEED
+#define MAX_SPEED 2300               //MAX_SPEED
 #define Ki_INV 5.0
 //*******************************************************
 void state();                        //action of the next state
@@ -24,6 +29,7 @@ void NextStep_REVERES();			 //what is the next step reveres the motor direction
 void MOTOR_STOP();                   //stop the motor
 void BLDC_init();
 void read_string();
+void read_string_MOTOR2ENCODER();
 //*******************************************************
 
 uint8_t hall1state=1;                 //hall sensor
@@ -34,19 +40,22 @@ uint8_t MOTOR_state =0;              //motor state
 uint8_t MOTOR_DIRECTION;            //motor direction
 
 uint32_t SET_MOTOR_SPEED;
-int encoder_pulses;
+long long encoder_pulses;
 int MOTOR_SPEED_RPM,last_read_motor_speed;
 int error,MOTOR_PWM;
 
 uint32_t TIME_CALC;
 
-uint8_t buf[7];
+char buf[50];
 uint8_t stop_flage,flag_SPEED;
 uint8_t HALL2READ;
 
 unsigned char ch;
-char string_data[10];
+unsigned char ch2;
+char string_data[15];
+char string_data2[35];
 uint8_t serial_flag=0,index1;
+uint8_t serial_flag2=0,index2;
 //=================
 uint8_t system_count =0;
 //*******************************************************
@@ -99,6 +108,19 @@ void s2(){
 void s3(){
 	//hall read
 	hall3state=READ_PIN(GPIOA, pin2);
+
+#ifndef Encoder_16_P
+	//calculate the distance
+	HALL2READ = READ_PIN(GPIOA, pin1);
+	if(hall3state==1){
+		if(HALL2READ > 0){
+			encoder_pulses--;
+		}
+		else{
+			encoder_pulses++;
+		}
+	}
+#endif
 	//motor direction
 	if(MOTOR_DIRECTION==0){
 		NextStep_NORMAL();
@@ -300,18 +322,24 @@ void MOTOR_STOP(){
 //*******************************************************
 void BLDC_init(){
 	MCAL_speed_init(CLK_SORC_IN_CLK8MHz, mood_FAST_MOOD_36MHzCORE);
+#ifdef MOTOR_MASTER
+	USART_INIT(USART2, mode_RX_TX_ENABLE, word_length_8, stop_bits_1, baud_rate_9600, parity_none,clk);
+	//to receive the data from slave motor control
+#endif
+	//motor control pins
 	pinmode(GPIOB, pin0,GPIO_MODE_OUTPUT_push_pull_Speed10);
 	pinmode(GPIOB, pin1,GPIO_MODE_OUTPUT_push_pull_Speed10);
 	pinmode(GPIOB, pin10,GPIO_MODE_OUTPUT_push_pull_Speed10);
 	pinmode(GPIOB, pin7,GPIO_MODE_OUTPUT_push_pull_Speed10);
 	pinmode(GPIOB, pin8,GPIO_MODE_OUTPUT_push_pull_Speed10);
 	pinmode(GPIOB, pin5,GPIO_MODE_OUTPUT_push_pull_Speed10);
-	ADC_INIT(ADC1, ADC_STATE_START);
+	//encoder interrupt pins
 	EXTI(GPIOA, pin0,EXTI_Trigger_RisingAndFalling, s1);
 	EXTI(GPIOA, pin1,EXTI_Trigger_RisingAndFalling, s2);
 	EXTI(GPIOA, pin2,EXTI_Trigger_RisingAndFalling, s3);
+	//send data to ttl on master , send encoder and velocity data on slave
 	USART_INIT(USART1, mode_RX_TX_ENABLE, word_length_8, stop_bits_1, baud_rate_9600, parity_none,clk);
-
+	//start motor sequence
 	s1();
 	s2();
 	s3();
@@ -320,7 +348,11 @@ void BLDC_init(){
 		MOTOR_PWM+=1;
 		delay(2, U_ms, clk);
 	}
+	//Receive the velocity & direction command
 	USART_READ_INTERRUPT_EN(USART1, read_string);
+#ifdef MOTOR_MASTER
+	USART_READ_INTERRUPT_EN(USART2, read_string_MOTOR2ENCODER);
+#endif
 }
 //*******************************************************
 //==================== -READ_DATA- ======================
@@ -345,6 +377,33 @@ void read_string (){
 		index1++;
 	}
 }
+#ifdef MOTOR_MASTER
+//##COMENT it in motor 2
+//*******************************************************
+//==================== -READ_MOTOR(2)ENCODER_DATA- ======================
+//*******************************************************
+void read_string_MOTOR2ENCODER (){
+
+	USART_READ(USART2,&ch2);
+
+	if(ch2=='C'){
+		serial_flag2=1;
+		index2=0;
+	}
+	if(ch2=='*'){
+		serial_flag2=0;
+		string_data2[index2]='\n';
+		string_data2[index2+1]=0;
+
+	}
+
+	if(serial_flag2==1){
+		string_data2[index2]=ch2;
+		index2++;
+	}
+}
+//###
+#endif
 //*******************************************************
 //==================== -MAIN Fun- =======================
 //*******************************************************
@@ -355,7 +414,7 @@ int main (){
 	BLDC_init();
 
 	while(1){
-		delay(90, U_ms, clk);
+		delay(SYSTEM_DELAY, U_ms, clk);
 		system_count++;
 		//******************************
 		//======== -speed- ============
@@ -403,19 +462,39 @@ int main (){
 		//******************************
 		//======== -direction- ========
 		//******************************
-		if(string_data[2]=='1'){//if motor 2 string_data[0]
+#ifdef MOTOR_MASTER
+		if(string_data[2]=='1'){//if motor 2 string_data[1]
 			MOTOR_DIRECTION=0;
 		}
 		else {
 			MOTOR_DIRECTION=1;
 		}
+#endif
+#ifndef MOTOR_MASTER
+		if(string_data[1]=='1'){//if motor 2 string_data[1]
+			MOTOR_DIRECTION=0;
+		}
+		else {
+			MOTOR_DIRECTION=1;
+		}
+#endif
 
+		//******************************
+		//======== -display- ========
+		//******************************
 		if(system_count>1){
-			//display
-			sprintf (buf, "%d",MOTOR_SPEED_RPM/10);
+#ifdef MOTOR_MASTER
+			//master
+			sprintf (buf, "R=%d,C1=%ld,",MOTOR_SPEED_RPM/10,encoder_pulses);
+			strcat(buf, string_data2);
+			strcat(buf, "\n");
 			USART_SEND_STRING(USART1,buf);
-			delay(3, U_ms, clk);
-			USART_SEND_STRING(USART1," RPM \n");
+#endif
+#ifndef MOTOR_MASTER
+			sprintf (buf, "C2=%ld*",encoder_pulses);
+			USART_SEND_STRING(USART1,buf);
+#endif
+
 			system_count=0;
 		}
 		MOTOR_SPEED_RPM=0;
